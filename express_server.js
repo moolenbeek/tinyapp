0464dc5d3910c30f4bf0186dff728a879280ae68
Eric Moolenbeek <emoolenbeek@gmail.com>
2021-12-06 18:48:56 +0000

    add long url

--- express_server.js
+++ express_server.js
@@ -36,6 +36,9 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL};
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL]
+  };
   res.render("urls_show", templateVars);
 });
